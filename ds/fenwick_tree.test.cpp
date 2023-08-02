9523ee365d82b2e7297929198d94f322e8794cea
primenumber <prime@kmc.gr.jp>
2023-08-02 11:41:26 +0900

    Apply formatter

--- ds/fenwick_tree.test.cpp
+++ ds/fenwick_tree.test.cpp
@@ -1,15 +1,20 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/point_add_range_sum"
 #include <iostream>
 #include <vector>
+
 #include "ds/fenwick_tree.hpp"
 
-#define rep2(i,k,n) for(i64 i=(i64)(k);i<(i64)(n);i++)
-#define rep(i,n) rep2(i,0,n)
-#define all(x) begin(x),end(x)
+#define rep2(i, k, n) for (i64 i = (i64)(k); i < (i64)(n); i++)
+#define rep(i, n) rep2(i, 0, n)
+#define all(x) begin(x), end(x)
 #ifdef ENV_LOCAL
-#define dump if (1) cerr
+#define dump                                                                                                           \
+  if (1)                                                                                                               \
+  cerr
 #else
-#define dump if (0) cerr
+#define dump                                                                                                           \
+  if (0)                                                                                                               \
+  cerr
 #endif
 
 using namespace std;
@@ -25,26 +30,26 @@ using vi64 = vector<i64>;
 using namespace harudake;
 
 int main() {
-  i64 n,q;
-  cin>>n>>q;
-  fenwick_tree bit(n);
-  rep(i,n) {
+  i64 n, q;
+  cin >> n >> q;
+  fenwick_tree bit(n + 1);
+  rep(i, n) {
     i64 a;
-    cin>>a;
+    cin >> a;
     bit.update(i, a);
   }
-  rep(qcnt,q) {
+  rep(qcnt, q) {
     i64 t;
-    cin>>t;
-    if(t==0) {
-      i64 p,x;
-      cin>>p>>x;
+    cin >> t;
+    if (t == 0) {
+      i64 p, x;
+      cin >> p >> x;
       bit.update(p, x);
     } else {
-      i64 l,r;
-      cin>>l>>r;
-      auto res = bit.query(r-1) - bit.query(l-1);
-      cout<<res<<"\n";
+      i64 l, r;
+      cin >> l >> r;
+      auto res = bit.query(r - 1) - bit.query(l - 1);
+      cout << res << "\n";
     }
   }
   return 0;
